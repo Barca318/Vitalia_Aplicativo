d4cc44295bb7446470dbce7d08e4e4c64bcee6e8
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-11-14 21:14:38 -0500

    metodo actualizar completo

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -59,6 +59,19 @@ public class Menu {
             System.out.print("Apellido paterno: "); String ap = sc.nextLine();
             System.out.print("Apellido materno: "); String am = sc.nextLine();
             System.out.print("Estado civil: "); String ec = sc.nextLine();
+            
+            CallableStatement cs = cn.prepareCall("{CALL sp_paciente_update(?,?,?,?,?)}");
+            cs.setInt(1, id);
+            cs.setString(2, nombres);
+            cs.setString(3, ap);
+            cs.setString(4, am);
+            cs.setString(5, ec);
+
+            ResultSet rs = cs.executeQuery();
+            while (rs.next()) System.out.println(rs.getString(1));
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
