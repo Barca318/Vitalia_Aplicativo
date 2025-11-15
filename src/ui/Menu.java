668ad0115b8ccc5b04ca46de38b3543c260c4a50
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-11-14 21:12:40 -0500

    metodo insertar paciente completo

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -28,6 +28,28 @@ public class Menu {
             }
         }
     }
-    
+
+    private static void insertarPaciente() {
+        try (Connection cn = Db.conectar()) {
+            System.out.print("DNI: "); String dni = sc.nextLine();
+            System.out.print("Nombres: "); String nombres = sc.nextLine();
+            System.out.print("Apellido paterno: "); String ap = sc.nextLine();
+            System.out.print("Apellido materno: "); String am = sc.nextLine();
+            System.out.print("Estado civil: "); String ec = sc.nextLine();
+
+            CallableStatement cs = cn.prepareCall("{CALL sp_paciente_insert(?,?,?,?,?)}");
+            cs.setString(1, dni);
+            cs.setString(2, nombres);
+            cs.setString(3, ap);
+            cs.setString(4, am);
+            cs.setString(5, ec);
+
+            ResultSet rs = cs.executeQuery();
+            while (rs.next()) System.out.println(rs.getString(1));
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
     
 }
