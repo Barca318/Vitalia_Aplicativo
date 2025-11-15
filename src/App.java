d2af1704cc9024bd23929c8912c7cef72c0eba0d
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-11-14 21:09:02 -0500

    arreglar errores

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class App {
 
    public static void main(String[] args){
-   Connection cn = DB.Db.conectar();
+   Connection cn = db.Db.conectar();
    if (cn !=null){
     try{
         Statement st = cn.createStatement();
